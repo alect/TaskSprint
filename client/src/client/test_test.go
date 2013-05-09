a1399793c38d58d569920a5d191dc75a3b40a912
Sergio Benitez <sbenitez@mit.edu>
2013-05-09 01:26:24 -0400

    Fixed timeout in test.

--- client/src/client/test_test.go
+++ client/src/client/test_test.go
@@ -117,7 +117,7 @@ func TestSimple(t *testing.T) {
   clients := CreateClients(numClient, kvh)
 
   // Run the computation, timeout in 15 seconds
-  Run(clients, nservers, sca, 30, true)
+  Run(clients, nservers, sca, 15, true)
 
   // Cleanup the coordinators
   cleanup(coa)
